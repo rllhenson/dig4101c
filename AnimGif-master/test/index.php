1a9347b393104b09cad02e9b234e98283e579b43
rllhenson <rllhenson@gmail.com>
2014-10-23 15:54:35 -0400

    can see the gif immediately

--- AnimGif-master/test/index.php
+++ AnimGif-master/test/index.php
@@ -9,7 +9,9 @@ $anim = new GifCreator\AnimGif();
 // Load all images from "./img/", in ascending order,
 // apply the given delays, and save the result...
 
-$anim	-> create("img/", array(300, 500)) // first 3s, then 5s for all the others
+$anim	-> create("img/", array(100, 100)) // first 3s, then 5s for all the others
 	-> save("anim.gif");
+
+echo "<img src='anim.gif'>";
 
 ?>
